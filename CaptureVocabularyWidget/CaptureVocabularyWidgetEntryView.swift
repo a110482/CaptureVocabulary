b7eb0681afa21089874dbf6380b8fc63bf75a1b1
elijah <a110482@yahoo.com.tw>
2023-04-13 18:29:26 +0800

    [feat] widget
    [feat] widget

--- CaptureVocabularyWidget/CaptureVocabularyWidgetEntryView.swift
+++ CaptureVocabularyWidget/CaptureVocabularyWidgetEntryView.swift
@@ -12,17 +12,69 @@ import Intents
 struct CaptureVocabularyWidgetEntryView : View {
     var entry: CaptureVocabularyWidgetProvider.Entry
     
+    init(entry: CaptureVocabularyWidgetProvider.Entry) {
+        self.entry = entry
+    }
+    
     @ViewBuilder
     var body: some View {
-//        Text(entry.date, style: .time)
-        Label("hello", image: "")
+        ZStack {
+            VStack {
+                Text(entry.vocabularyCard?.normalizedSource ?? "")
+                    .font(.title)
+                    .foregroundColor(.black)
+                
+                Color.black.frame(height: 1)
+                    .padding([.leading, .trailing], 20)
+                
+                Text(entry.vocabularyCard?.normalizedTarget ?? "")
+                    .font(.headline)
+                    .foregroundColor(.black)
+            }
+        }
+        .background(Color.white)
+    }
+}
+
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+
+        let r = Double((rgbValue & 0xFF0000) >> 16) / 255.0
+        let g = Double((rgbValue & 0x00FF00) >> 8) / 255.0
+        let b = Double(rgbValue & 0x0000FF) / 255.0
+
+        self.init(red: r, green: g, blue: b)
+    }
+}
+
+struct CaptureVocabularyWidgetEntryView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            CaptureVocabularyWidgetEntryView(
+                entry: CaptureVocabularyWidgetEntry(
+                    date: Date(),
+                    configuration: ConfigurationIntent()))
+        }
+        .previewContext(WidgetPreviewContext(family: .systemMedium))
     }
 }
 
 // MARK: -
 struct CaptureVocabularyWidgetEntry: TimelineEntry {
-    let date: Date
+    var date: Date
     let configuration: ConfigurationIntent
+    let vocabularyCard: VocabularyCardORM.ORM? = {
+        let numbers = VocabularyCardORM.ORM.cardNumbers(memorized: false)
+        guard numbers > 0 else { return nil }
+        let randomIndex = Int.random(in: 0 ..< numbers)
+        let card =  VocabularyCardORM.ORM.get(by: randomIndex, memorized: false)
+        return card
+    }()
+    
 }
 
 // MARK: -
@@ -30,25 +82,39 @@ struct CaptureVocabularyWidgetEntry: TimelineEntry {
 struct CaptureVocabularyWidgetProvider: IntentTimelineProvider {
     typealias Entry = CaptureVocabularyWidgetEntry
     func placeholder(in context: Context) -> Entry {
-        Entry(date: Date(), configuration: ConfigurationIntent())
+        Entry(date: Date(),
+              configuration: ConfigurationIntent()
+        )
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Entry) -> ()) {
-        let entry = Entry(date: Date(), configuration: configuration)
+        let entry = Entry(date: Date(),
+                          configuration: configuration
+        )
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [Entry] = []
-
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = Entry(date: entryDate, configuration: configuration)
-            entries.append(entry)
+        var currentDate = Date()
+        var entries: [Entry] = [
+            Entry(date: currentDate, configuration: configuration),
+            Entry(date: currentDate, configuration: configuration),
+            Entry(date: currentDate, configuration: configuration),
+        ]
+        
+        
+        let calendar = Calendar.current
+        for index in entries.indices {
+            if let modifiedDate = calendar.date(
+                byAdding: .second,
+                value: 30,
+                to: currentDate) {
+                currentDate = modifiedDate
+                entries[index].date = currentDate
+            }
         }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        
+        let timeline = Timeline(entries: entries, policy: .after(currentDate))
         completion(timeline)
     }
 }
