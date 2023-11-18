d633eb0fb8855172a615802f54c07737b3fb88da
elijah <a110482@yahoo.com.tw>
2023-11-18 20:33:41 +0800

    [feat] 單字卡編輯功能

--- CaptureVocabulary/VocabularyCards/VocabularyCardsCoordinator.swift
+++ CaptureVocabulary/VocabularyCards/VocabularyCardsCoordinator.swift
@@ -17,6 +17,8 @@ class VocabularyCardsCoordinator: Coordinator<UINavigationController> {
     private var viewController: VocabularyCardsViewController!
     private var viewModel: VocabularyCardsViewModel!
     private let selectedList: VocabularyCardListORM.ORM
+    private let disposeBag = DisposeBag()
+    private var childActionDisposeBag: DisposeBag? = DisposeBag()
     
     init(rootViewController: UINavigationController, selectedList: VocabularyCardListORM.ORM) {
         self.selectedList = selectedList
@@ -26,6 +28,13 @@ class VocabularyCardsCoordinator: Coordinator<UINavigationController> {
     override func start() {
         guard !started else { return }
         viewController = VocabularyCardsViewController()
+        viewController.action.subscribe(onNext: { [weak self] action in
+            guard let self = self else { return }
+            switch action {
+            case .selectedCell(let cardModel):
+                presentCreateVocabularyCoordinator(cardModel: cardModel)
+            }
+        }).disposed(by: disposeBag)
         viewModel = VocabularyCardsViewModel(selectedList: selectedList)
         viewController.bind(viewModel)
         show(viewController: viewController)
@@ -35,5 +44,26 @@ class VocabularyCardsCoordinator: Coordinator<UINavigationController> {
     @available(*, unavailable)
     public required init(rootViewController: UINavigationController) {
         fatalError("init(rootViewController:) has not been implemented")
+    }
+}
+
+private extension VocabularyCardsCoordinator {
+    func presentCreateVocabularyCoordinator(cardModel: VocabularyCardORM.ORM) {
+        let coordinator = EditVocabularyCardsCoordinator(
+            rootViewController: viewController,
+            cardModel: cardModel
+        )
+        startChild(coordinator: coordinator)
+        childActionDisposeBag = DisposeBag()
+        coordinator.action.subscribe(onNext: { [weak self] action in
+            guard let self = self else { return }
+            self.childActionDisposeBag = nil
+            switch action {
+            case .dismiss:
+                break
+            case .saved:
+                viewModel.loadCards()
+            }
+        }).disposed(by: childActionDisposeBag!)
     }
 }
